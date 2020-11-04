24cadfd10a23a5133ac61ea87da6fad5b7aa02c4
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2020-11-04 13:12:43 -0600

    🦄 AWS Amplify && GraphQL && TypeScript && Go  🍰

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,8 @@
 import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
+
+// 🏖️ Inspired Problem Solver 🚀 Visual Storyteller 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
